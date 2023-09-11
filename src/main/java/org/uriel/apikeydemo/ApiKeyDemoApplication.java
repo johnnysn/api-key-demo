85d5866cb4157fff92bc224106a4f7245c3ca1bb
johnnysn <johnnysn@gmail.com>
2023-09-11 15:20:34 -0300

    First commit

---
+++ src/main/java/org/uriel/apikeydemo/ApiKeyDemoApplication.java
@@ -0,0 +1,13 @@
+package org.uriel.apikeydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKeyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uriel/apikeydemo/ApiKeyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uriel.apikeydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
